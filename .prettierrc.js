9e75256b235b808577f0c2ee345c7c575eb926e9
Dan White <d.white@zengenti.com>
2023-11-23 11:36:24 +0000

    added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 160,
+    trailingComma: 'none',
+    singleQuote: true,
+    proseWrap: 'always',
+    overrides: [
+        {
+            files: ['*.ts', '*.html', '*.json', '*.js', '*.scss']
+        }
+    ]
+};
