2b01ca527c9887838f54406b76c703f0d7514976
Neil Flatley <n.flatley@zengenti.com>
2023-11-23 11:50:43 +0000

    build: add cjs / esm module build script to `canvas-react`, each bundle type referenced in `package.json` for a consumer to use with their respective build/runtime

---
+++ packages/react/tsup.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: {
+        ['canvas-react']: 'src/index.ts'
+    },
+    format: ['esm', 'cjs'],
+    // this runs tsc to do type checking during build but the console 
+    // output is not as readable as vanilla tsc when there are problems to resolve
+    //   dts: true,
+    sourcemap: true,
+    clean: true
+});
