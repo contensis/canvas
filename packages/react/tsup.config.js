4470cb829975cd6f79a7ca6f4018c5f9de09cf09
Neil Flatley <n.flatley@zengenti.com>
2024-05-02 10:45:20 +0100

    build: avoid .mjs extension in esm builds to prevent error `Can't import the named export 'useContext' from non EcmaScript module` in legacy consumer webpack builds

--- packages/react/tsup.config.js
+++ packages/react/tsup.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     },
     format: ['esm', 'cjs'],
     target: 'es6',
+    legacyOutput: true, // outputs esm to folder avoiding .mjs file extension
     // this runs tsc to do type checking during build but the console
     // output is not as readable as vanilla tsc when there are problems to resolve
     //   dts: true,
