aacbae87695e8b965445b9957fc99b65563b61df
Neil Flatley <n.flatley@zengenti.com>
2024-08-09 16:37:18 +0100

    build: use .mjs extension for ESM build - reverts change in v1.0.5 and fixes #20 `Cannot use import statement outside a module` when project specifies `type: module`

--- packages/react/tsup.config.js
+++ packages/react/tsup.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     },
     format: ['esm', 'cjs'],
     target: 'es6',
-    legacyOutput: true, // outputs esm to folder avoiding .mjs file extension
     // this runs tsc to do type checking during build but the console
     // output is not as readable as vanilla tsc when there are problems to resolve
     //   dts: true,
